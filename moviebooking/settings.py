8fbcec32195ade349677a0e9ec86ee26e02ce198
Sivakumar089 <kumarsiva200426@gmail.com>
2025-07-21 13:51:40 +0530

    .settings file database addedd

--- moviebooking/settings.py
+++ moviebooking/settings.py
@@ -96,23 +96,23 @@ WSGI_APPLICATION = 'moviebooking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':os.getenv('DATABASE_NAME'),
-#         'USER':os.getenv('DATABASE_USERNAME') ,
-#         'PASSWORD':os.getenv('DATABASE_PASSWORD') ,
-#         'HOST': os.getenv('DATABASE_HOSTNAME'),
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':os.getenv('DATABASE_NAME'),
+        'USER':os.getenv('DATABASE_USERNAME') ,
+        'PASSWORD':os.getenv('DATABASE_PASSWORD') ,
+        'HOST': os.getenv('DATABASE_HOSTNAME'),
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
